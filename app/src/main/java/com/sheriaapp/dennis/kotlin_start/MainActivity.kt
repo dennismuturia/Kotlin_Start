8450ef934905719f7bb684a1c9d74db247b52fcd
dennismuturia <dmuturia2922@gmail.com>
2017-11-12 12:00:25 +0300

    Integrated Anko. Anko is a kotlin library making android development easy

--- app/src/main/java/com/sheriaapp/dennis/kotlin_start/MainActivity.kt
+++ app/src/main/java/com/sheriaapp/dennis/kotlin_start/MainActivity.kt
@@ -2,11 +2,18 @@ package com.sheriaapp.dennis.kotlin_start
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import butterknife.Bind
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+//This will be for clicking
+    override fun onClick(p0: View?) {
+
+    }
+
+
 }
